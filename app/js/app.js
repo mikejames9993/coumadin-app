f3dcba78e3648c1b23754f6d066b75da169c6eb3
Jon Football <coumadinapp@gmail.com>
2017-02-20 15:53:28 -0500

    jons first commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,116 @@
 'use strict';
 
 var app = angular.module('CoumadinApp', [
-    'ngRoute'
+    'ngRoute', 'permission', 'permission.ng',
 ]);
 
 app.config(function($routeProvider) {
 
     $routeProvider
         .when('/', {
-            templateUrl : 'main.html'
+            templateUrl: '/views/main.html',
+            // data: {
+            //     permissions: {
+            //         only: ['AUTHENTICATED'],
+            //         redirectTo: '/login'
+            //     }
+            // }
+        })
+        .when('/login', {
+            templateUrl: '/views/login.html',
+            // data: {
+            //     permissions: {
+            //         except: ['AUTHENTICATED'],
+            //         redirectTo: '/'
+            //     }
+            // }
         })
         .when('/scenario', {
-            templateUrl : '/views/scenarios/scenario.html'
-        });
-});
+            templateUrl: '/views/scenarios/scenario.html',
+            // data: {
+            //     permissions: {
+            //         only: ['AUTHENTICATED'],
+            //         redirectTo: '/login'
+            //     }
+            // }
+        })
+        .otherwise({ redirectTo: '/' });
+})
+
+// app.run(function(PermPermissionStore, PermRoleStore, $q) {
+
+
+//     function Session(deferred) {
+//         deferred.resolve();
+//         // function checkSession() {
+//         //     console.log('checkSession');
+
+//         // }
+//         // return {
+//         // 	checkSession: checkSession
+//         // };
+//         console.log(deferred);
+//         return deferred.promise;
+//     }
+
+//     var roleConfig = {
+//         'AUTHENTICATED': $q.defer()
+//     };
+
+
+//     PermRoleStore
+//     // Or use your own function/service to validate role
+//         .defineRole(roleConfig, function(deferred) {
+//         return deferred.promise; //Session;//.checkSession();
+//     });
+//         // deferred.reject();
+
+//     setTimeout(function(deferred) { 
+//     	console.log('timeout fired');
+//         deferred.resolve(); 
+//     }, 3000);
+
+//     // var roleConfig = {
+//     //     'Admin': $q.defer(),
+//     //     'Auditor': $q.defer(),
+//     //     'QA': $q.defer(),
+//     //     'Lead': $q.defer(),
+//     //     'Clerk': $q.defer(),
+//     //     'Manager': $q.defer(),
+//     //     'Internal': $q.defer(),
+//     //     'External': $q.defer()
+//     // };
+//     // var roles = {};
+//     // _.each(roleConfig, function(deferred, roleName) {
+//     //     roles[roleName] = function() {
+//     //         return deferred.promise;
+//     //     };
+//     // });
+
+//     // PermRoleStore.defineManyRoles(roles);
+
+//     // webservices.metadata.getLoggedInUser().then(function(response) {
+
+//     //         _.each(roleConfig, function(deferred, roleName) {
+//     //             var matchingRole = _.find(response.data.assignedRoles, function(role) {
+//     //                 return role.name === roleName;
+//     //             });
+//     //             if (matchingRole) {
+//     //                 deferred.resolve();
+//     //             } else {
+//     //                 deferred.reject();
+//     //             }
+//     //         });
+//     //         angular.element('#main-tab-content').removeAttr('hidden');
+//     //         angular.element('#main-tabs').removeAttr('hidden');
+
+//     //     },
+//     //     function(response) {
+//     //         console.log("Unable to fetch role list on login: " + response.error.message);
+//     //         angular.element('#permissions-error-msg').removeAttr('hidden');
+//     //     });
+
+// });
+
+
+;
--- app/js/root-controller.js
+++ app/js/root-controller.js
@@ -2,4 +2,8 @@
 
 angular.module('CoumadinApp').controller('RootController', function($rootScope, $scope) {
 	console.log('root controller loading');
+	$rootScope.user = {
+		username: "",
+		password: ""
+	};
 });
