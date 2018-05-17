876f8f3ac3144f24ec563ea0ca6778e16163f509
Mathis <mmathis124@gmail.com>
2018-05-16 23:28:29 -0400

    prevent user from selecting multiple ptinr reasons if one of the reasons is that they forgot

--- app/components/scenarios/scenario-coumadin-controller.js
+++ app/components/scenarios/scenario-coumadin-controller.js
@@ -10,13 +10,21 @@ angular.module('CoumadinApp').controller('ScenarioCoumadinController', function(
 
 	$scope.showRules = function() {
 		if (data.config.id === 'pt-inr-monitoring') {
-			var NoReasonSelected = Object.keys(data.custom.coumadinReasons).every(k => !data.custom.coumadinReasons[k]);
-			if (NoReasonSelected) { //If no reason selected, show warning
+			var numReasonsSelected = _.reduce(data.custom.coumadinReasons, function(memo, reason) {
+				return memo + (reason === true ? 1 : 0);
+			});
+			// Object.keys(data.custom.coumadinReasons).every(k => !data.custom.coumadinReasons[k]);
+			if (numReasonsSelected === 0) { //If no reason selected, show warning
 				$rootScope.showMessage({
 					type: 'warning',
 					text: "Please Select a Reason Before Continuing."
 				});
-			} 
+			} else if (numReasonsSelected > 1 && data.custom.coumadinReasons.other) {
+				$rootScope.showMessage({
+					type: 'warning',
+					text: "You may not choose multiple reasons if one of them is that you have forgotten or don't know."
+				});
+			}
 			else {
 				navigation.showRules();
 				$rootScope.hideMessage('warning');
