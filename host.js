3a2a5929130a7e7e7fc0c665c32db4bd10762ba6
mikejames9993 <mikejames9993@gmail.com>
2019-03-14 23:35:36 -0400

    add default port

--- host.js
+++ host.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 
 // Read port from shell arguments
-var port = process.argv[2];
+var port = process.argv[2] || 3005;
 
 var app = express();
 
